61d7fe5d5e0d4050b39e40ec837724dd7cbd5ab4
voydevich <voydevichb@gmail.com>
2023-08-01 21:26:52 +0300

    profile orders and ordered

--- src/app/profile/ordered/page.js
+++ src/app/profile/ordered/page.js
@@ -1,7 +1,59 @@
+'use client'
+import style from '../profile.module.scss'
+import {getOrderedOrders} from "@/app/profile/actions"
+import {useEffect, useState} from "react"
+import {useUserContext} from "@/app/providers/firebase/UserProvider"
+import OrderItem from "@/app/components/order-item/OrderItem"
+import Empty from "@/app/profile/empty"
+
 const OrderedPage = () => {
+    const {user} = useUserContext()
+
+    const [isLoading, setLoading] = useState(true)
+    const [orders, setOrders] = useState({})
+
+    useEffect(() => {
+        if (user?.accessToken) getOrderedOrders(user.accessToken).then((orders) => {
+            if (orders) {
+                setOrders(orders)
+            }
+            setLoading(false)
+        })
+    }, [user.accessToken])
+
+    if (isLoading) return "..loading"
+    if (!orders?.paid?.orders?.length && !orders?.completed?.orders?.length) {
+        return (<Empty/>)
+    }
     return (
         <div>
-            OrderedPage
+            {!!orders?.paid?.orders?.length && (
+                <>
+                    <div className={style.subtitle}>
+                        Оплачені
+                    </div>
+                    <div className={style.orders}>
+                        {orders.paid.orders.map((order) => (
+                            <OrderItem order={order} key={order.id} ordered={true}/>
+                        ))}
+                    </div>
+                </>
+
+            )}
+            {!!orders?.completed?.orders?.length && (
+                // {!!orders?.paid?.orders?.length && (
+                <>
+                    <div className={style.subtitle}>
+                        Виконані
+                    </div>
+                    <div className={style.orders}>
+                        {orders?.completed?.orders.map((order) => (
+                            <OrderItem order={order} key={order.id}/>
+                        ))}
+                    </div>
+                </>
+
+            )}
         </div>
     )
 }
