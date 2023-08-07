edcbc88285252e12b85ca5b54c5ee19be6d6b627
voydevich <voydevichb@gmail.com>
2023-08-07 11:03:38 +0300

    error

--- src/app/(static)/policy/page.js
+++ src/app/(static)/policy/page.js
@@ -1,5 +1,4 @@
-
-import style from './policy.module.scss';
+import style from "./policy.module.scss"
 
 const PolicyPage = () => {
     return (
@@ -14,7 +13,15 @@ const PolicyPage = () => {
                         H2. Title here
                     </h2>
                     <p>
-                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                        It is a long established fact that a reader will be distracted by the readable content of a page
+                        when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                        distribution of letters, as opposed to using &apos;Content here, content here&apos;, making it
+                        look like
+                        readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as
+                        their default model text, and a search for &apos;lorem ipsum&apos; will uncover many web sites
+                        still in
+                        their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on
+                        purpose (injected humour and the like).
                     </p>
                     <h3>
                         H3. Title here
@@ -26,7 +33,15 @@ const PolicyPage = () => {
                         <li>distracted by the readable</li>
                     </ol>
                     <p>
-                        <strong>It is a long established fact</strong> that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                        <strong>It is a long established fact</strong> that a reader will be distracted by the readable
+                        content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                        more-or-less normal distribution of letters, as opposed to using &apos;Content here, content
+                        here&apos;,
+                        making it look like readable English. Many desktop publishing packages and web page editors now
+                        use Lorem Ipsum as their default model text, and a search for &apos;lorem ipsum&apos; will
+                        uncover many
+                        web sites still in their infancy. Various versions have evolved over the years, sometimes by
+                        accident, sometimes on purpose (injected humour and the like).
                     </p>
                     <h2>
                         <i>H2. Title here</i>
@@ -37,7 +52,15 @@ const PolicyPage = () => {
                     </ul>
                     <span>Test</span>
                     <p>
-                        <a href="#">It is a long established fact that a reader</a> will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                        <a href="#">It is a long established fact that a reader</a> will be distracted by the readable
+                        content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                        more-or-less normal distribution of letters, as opposed to using &apos;Content here, content
+                        here&apos;,
+                        making it look like readable English. Many desktop publishing packages and web page editors now
+                        use Lorem Ipsum as their default model text, and a search for &apos;lorem ipsum&apos; will
+                        uncover many
+                        web sites still in their infancy. Various versions have evolved over the years, sometimes by
+                        accident, sometimes on purpose (injected humour and the like).
                     </p>
                 </div>
             </div>
