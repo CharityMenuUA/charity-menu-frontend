45e3a0f3d84a8b19624195192ed98ebfe98c4810
voydevich <voydevichb@gmail.com>
2023-08-08 19:33:29 +0300

    sponsor

--- src/app/components/steps-slider/StepsSlider.js
+++ src/app/components/steps-slider/StepsSlider.js
@@ -136,7 +136,7 @@ const StepsSlider = () => {
 
     const configContext = useConfigContext()
     const {config: {fund, sponsor}} = configContext
-
+console.log(sponsor)
     return (
         <>
             {sponsor && (
@@ -157,8 +157,8 @@ const StepsSlider = () => {
                     <div className={style.sponsor_info}>
                         <p>Партнер цього місяця</p>
                         <div className={style.socials}>
-                            {sponsor?.instagram && (
-                                <a href={sponsor?.instagram} className={style.socials_icon} target="_blank">
+                            {sponsor?.contacts?.instagram && (
+                                <a href={sponsor?.contacts?.instagram} className={style.socials_icon} target="_blank">
                                     <svg width="40" height="40" viewBox="0 0 40 40">
                                         <path
                                             d="M25.5 9H14.5C11.42 9 9 11.42 9 14.5V25.5C9 28.58 11.42 31 14.5 31H25.5C28.58 31 31 28.58 31 25.5V14.5C31 11.42 28.58 9 25.5 9ZM28.8 25.5C28.8 27.37 27.37 28.8 25.5 28.8H14.5C12.63 28.8 11.2 27.37 11.2 25.5V14.5C11.2 12.63 12.63 11.2 14.5 11.2H25.5C27.37 11.2 28.8 12.63 28.8 14.5V25.5Z"
@@ -172,8 +172,8 @@ const StepsSlider = () => {
                                     </svg>
                                 </a>
                             )}
-                            {sponsor?.twitter && (
-                                <a href={sponsor?.twitter} className={style.socials_icon} target="_blank">
+                            {sponsor?.contacts?.twitter && (
+                                <a href={sponsor?.contacts?.twitter} className={style.socials_icon} target="_blank">
                                     <svg width="40" height="40" viewBox="0 0 40 40">
                                         <path
                                             d="M30.82 11.3398C29.93 11.8698 28.95 12.2498 27.9 12.4598C27.06 11.5698 25.86 11.0098 24.54 11.0098C22 11.0098 19.94 13.0698 19.94 15.6098C19.94 15.9698 19.98 16.3198 20.06 16.6598C16.22 16.4598 12.83 14.6298 10.56 11.8398C10.16 12.5198 9.94 13.3098 9.94 14.1598C9.94 15.7598 10.75 17.1698 11.99 17.9898C11.24 17.9698 10.53 17.7598 9.91 17.4098C9.91 17.4298 9.91 17.4498 9.91 17.4698C9.91 19.6998 11.5 21.5598 13.6 21.9798C13.21 22.0898 12.81 22.1398 12.39 22.1398C12.09 22.1398 11.81 22.1098 11.52 22.0598C12.11 23.8898 13.81 25.2198 15.82 25.2598C14.24 26.4898 12.26 27.2298 10.1 27.2298C9.73 27.2298 9.36 27.2098 9 27.1698C11.04 28.4798 13.46 29.2398 16.06 29.2398C24.53 29.2398 29.16 22.2298 29.16 16.1398C29.16 15.9398 29.16 15.7398 29.15 15.5398C30.05 14.8898 30.83 14.0798 31.44 13.1598C30.62 13.5298 29.73 13.7698 28.8 13.8798C29.75 13.3098 30.48 12.4098 30.83 11.3298L30.82 11.3398Z"
@@ -181,8 +181,8 @@ const StepsSlider = () => {
                                     </svg>
                                 </a>
                             )}
-                            {sponsor?.facebook && (
-                                <a href={sponsor?.facebook} className={style.socials_icon} target="_blank">
+                            {sponsor?.contacts?.facebook && (
+                                <a href={sponsor?.contacts?.facebook} className={style.socials_icon} target="_blank">
                                     <svg width="40" height="40" viewBox="0 0 40 40">
                                         <path
                                             d="M22.12 14.1C22.12 13.19 22.72 12.98 23.14 12.98H25.73V9.00003H22.16C18.2 8.99003 17.29 11.95 17.29 13.85V16.5H15V20.6H17.29V32.2H22.11V20.6H25.36L25.78 16.5H22.11V14.09L22.12 14.1Z"
@@ -190,8 +190,8 @@ const StepsSlider = () => {
                                     </svg>
                                 </a>
                             )}
-                            {sponsor?.telegram && (
-                                <a href={sponsor?.telegram} className={style.socials_icon} target="_blank">
+                            {sponsor?.contacts?.telegram && (
+                                <a href={sponsor?.contacts?.telegram} className={style.socials_icon} target="_blank">
                                     <svg width="40" height="40" viewBox="0 0 40 40">
                                         <path
                                             d="M28.303 12.0896L9.01298 19.5296C7.69298 20.0496 7.71298 20.7796 8.78298 21.1196L13.723 22.6596L25.173 15.4496C25.703 15.0996 26.203 15.2996 25.793 15.6496L16.523 24.0196L16.173 29.1096C16.693 29.1096 16.913 28.8896 17.183 28.6196L19.593 26.2996L24.593 29.9796C25.503 30.4996 26.153 30.2296 26.403 29.1296L29.683 13.6596C29.973 12.3096 29.193 11.7696 28.293 12.0896H28.303Z"
@@ -199,8 +199,8 @@ const StepsSlider = () => {
                                     </svg>
                                 </a>
                             )}
-                            {sponsor?.linkedin && (
-                                <a href={sponsor?.linkedin} className={style.socials_icon} target="_blank">
+                            {sponsor?.contacts?.linkedin && (
+                                <a href={sponsor?.contacts?.linkedin} className={style.socials_icon} target="_blank">
                                     <svg width="40" height="40" viewBox="0 0 40 40">
                                         <path
                                             d="M28.7237 9H10.5763C9.70834 9 9 9.68422 9 10.5271V28.7729C9 29.6158 9.70834 30.3 10.5763 30.3H28.7237C29.5917 30.3 30.3 29.6158 30.3 28.7729V10.5271C30.3 9.68422 29.5917 9 28.7237 9ZM15.385 26.6806H12.1726V17.0619H15.385V26.6806ZM13.7788 15.743H13.7588C12.6814 15.743 11.983 14.9993 11.983 14.0771C11.983 13.1351 12.7013 12.4112 13.7987 12.4112C14.8962 12.4112 15.5746 13.1251 15.5945 14.0771C15.6045 15.0092 14.9061 15.743 13.7788 15.743ZM26.9878 26.6707H23.7753V21.5242C23.7753 20.2251 23.3064 19.3525 22.1491 19.3525C21.2612 19.3525 20.7325 19.9475 20.503 20.5226C20.4132 20.7309 20.3933 21.0184 20.3933 21.306V26.6806H17.1708C17.1708 26.6806 17.2107 17.9642 17.1708 17.0619H20.3833V18.4204C20.8123 17.7659 21.5705 16.8338 23.2765 16.8338C25.3915 16.8338 26.9778 18.2122 26.9778 21.1573L26.9878 26.6707Z"/>
