1de36dc54233b58bb999aca014972d8fdd5be133
voydevich <voydevichb@gmail.com>
2024-02-10 17:57:35 +0200

    image | description | become chef

--- src/app/(static)/reporting/reporting-funds/ReportingFunds.js
+++ src/app/(static)/reporting/reporting-funds/ReportingFunds.js
@@ -1,13 +1,13 @@
-import Image from "next/image";
-import style from './reportingFunds.module.scss';
-import numberWithSpaces from "@/helpers/numberWithSpaces";
+import Image from "next/image"
+import style from './reportingFunds.module.scss'
+import numberWithSpaces from "@/helpers/numberWithSpaces"
 
-const ReportingFunds = ({ reports }) => {
+const ReportingFunds = ({reports}) => {
 
     return (
         <div className={style.reportingFunds}>
             {reports.map((report, reportKey) => {
-                const { recipient: { logo, link }, amount } = report;
+                const {recipient: {logo, link}, amount, attachments} = report
                 return (
                     <div className={style.reportingFunds_item} key={reportKey}>
                         <a href={link} target="_blank" className={style.reportingFunds_item_link}>
@@ -18,7 +18,18 @@ const ReportingFunds = ({ reports }) => {
                                 height={150}
                             />
                         </a>
-
+                        <div className={style.attachments}>
+                            {attachments?.length && attachments.map((src, key) => (
+                                <a key={key} href={src} target="_blank">
+                                    <Image
+                                        src={src}
+                                        alt="fund-logo"
+                                        width={150}
+                                        height={150}
+                                    />
+                                </a>
+                            ))}
+                        </div>
                         <strong className={style.reportingFunds_item_count}>₴ {numberWithSpaces(amount)}</strong>
                     </div>
                 )
@@ -27,4 +38,4 @@ const ReportingFunds = ({ reports }) => {
     )
 }
 
-export default ReportingFunds;
+export default ReportingFunds
